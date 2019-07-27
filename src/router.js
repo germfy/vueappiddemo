49050ac13eb8c58ced0dbffbe487f2ad0f1528a4
German Dominguez <german@nubarronconsulting.com>
2019-07-27 17:04:52 -0500

    Cambios iniciales

--- src/router.js
+++ src/router.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
-      component: Home
+      component: () => import( /* webpackChunkName: "about" */ './views/Home.vue')
     },
     {
       path: '/about',
@@ -19,7 +17,15 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import( /* webpackChunkName: "about" */ './views/Login.vue')
     }
   ]
 })
